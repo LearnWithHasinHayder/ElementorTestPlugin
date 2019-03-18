a4b85840950d2fad07d9698f68193b68d28eb529
Hasin Hayder <countdraculla@gmail.com>
2019-03-18 19:50:12 +0600

    Elementor Test Plugin

---
+++ elementortestplugin.php
@@ -0,0 +1,147 @@
+<?php
+/*
+Plugin Name: Elementor Test Plugin
+Plugin URI:
+Description:
+Version: 1.0
+Author: LWHH
+Author URI: https://hasin.me
+License: GPLv2 or later
+Text Domain: elementortestplugin
+Domain Path: /languages/
+*/
+
+use \Elementor\Plugin as Plugin;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( __( "Direct Access is not allowed", 'elementortestplugin' ) );
+}
+
+final class ElementorTestExtension {
+
+	const VERSION = "1.0.0";
+	const MINIMUM_ELEMENTOR_VERSION = "2.0.0";
+	const MINIMUM_PHP_VERSION = "7.0";
+
+	private static $_instance = null;
+
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+
+	}
+
+	public function __construct() {
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+	}
+
+	public function init() {
+		load_plugin_textdomain( 'elementor-test-extension' );
+
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+
+			return;
+		}
+
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+
+			return;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+
+			return;
+		}
+
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+
+		add_action( "elementor/elements/categories_registered", [ $this, 'register_new_category' ] );
+	}
+
+	public function register_new_category($manager){
+		$manager->add_category('testcategory',[
+			'title'=>__('Test Category','elementortestplugin'),
+			'icon'=>'fa fa-image'
+		]);
+
+		$manager->add_category('sliders',[
+			'title'=>__('Sliders','elementortestplugin'),
+			'icon'=>'fa fa-video'
+		]);
+	}
+
+	public function init_widgets() {
+		require_once( __DIR__ . '/widgets/test-widget.php' );
+
+		// Register widget
+		Plugin::instance()->widgets_manager->register_widget_type( new \Elementor_Test_Widget() );
+
+	}
+
+	public function admin_notice_minimum_php_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementortestplugin' ),
+			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortestplugin' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'elementortestplugin' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	public function admin_notice_minimum_elementor_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementortestplugin' ),
+			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortestplugin' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementortestplugin' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	public function admin_notice_missing_main_plugin() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementortestplugin' ),
+			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortestplugin' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementortestplugin' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+
+	}
+
+	public function includes() {
+	}
+
+}
+
+ElementorTestExtension::instance();
---
+++ widgets/test-widget.php
@@ -0,0 +1,290 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gru
+ * Date: 2019-02-23
+ * Time: 21:28
+ */
+
+class Elementor_Test_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return "TestWidget";
+	}
+
+	public function get_title() {
+		return __( "TestWidget", 'elementortestplugin' );
+	}
+
+	public function get_icon() {
+		return 'fa fa-image';
+	}
+
+	public function get_categories() {
+		return array( 'general', 'testcategory', 'sliders' );
+	}
+
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Content', 'elementortestplugin' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'heading',
+			[
+				'label'       => __( 'Heading', 'elementortestplugin' ),
+				'type'        => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => __( 'Hello World', 'elementortestplugin' ),
+			]
+		);
+
+		$this->add_control(
+			'heading_description',
+			[
+				'label'       => __( 'Type Description', 'elementortestplugin' ),
+				'type'        => \Elementor\Controls_Manager::TEXTAREA,
+				'placeholder' => __( 'Description', 'elementortestplugin' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'position_section',
+			[
+				'label' => __( 'Position', 'elementortestplugin' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'alignment',
+			[
+				'label'     => __( 'Heading Alignment', 'elementortestplugin' ),
+				'type'      => \Elementor\Controls_Manager::SELECT,
+				'default'   => 'left',
+				'options'   => [
+					'left'   => __( 'Left', 'elementortestplugin' ),
+					'right'  => __( 'Right', 'elementortestplugin' ),
+					'center' => __( 'Center', 'elementortestplugin' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} h1.heading' => 'text-align: {{VALUE}}'
+				]
+			]
+
+		);
+		$this->add_control(
+			'description_alignment',
+			[
+				'label'     => __( 'Description Alignment', 'elementortestplugin' ),
+				'type'      => \Elementor\Controls_Manager::SELECT,
+				'default'   => 'left',
+				'options'   => [
+					'left'   => __( 'Left', 'elementortestplugin' ),
+					'right'  => __( 'Right', 'elementortestplugin' ),
+					'center' => __( 'Center', 'elementortestplugin' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} p' => 'text-align: {{VALUE}}'
+				]
+			]
+
+		);
+
+		$this->end_controls_section();
+
+
+		$this->start_controls_section(
+			'color_section',
+			[
+				'label' => __( 'Color', 'elementortestplugin' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'heading_color',
+			[
+				'label'     => __( 'Heading Color', 'elementortestplugin' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+//				'placeholder' => __( 'Hello World', 'elementortestplugin' ),
+				'default'   => '#224400',
+				'selectors' => [
+					'{{WRAPPER}} h1.heading' => 'color: {{VALUE}}'
+				]
+			]
+		);
+		$this->add_control(
+			'description_color',
+			[
+				'label'     => __( 'Description Color', 'elementortestplugin' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+//				'placeholder' => __( 'Hello World', 'elementortestplugin' ),
+				'default'   => '#888888',
+				'selectors' => [
+					'{{WRAPPER}} p' => 'color: {{VALUE}}'
+				]
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'image_section',
+			[
+				'label' => __( 'Image', 'elementortestplugin' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'imagex',
+			[
+				'label'   => __( 'Image', 'elementortestplugin' ),
+				'type'    => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src()
+				]
+			] );
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Image_Size::get_type(),
+			[
+				'default' => 'large',
+				'name'    => 'imagesz'
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'demo_section',
+			[
+				'label' => __( 'Control Demo', 'elementortestplugin' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'demo_select2',
+			[
+				'label'       => __( 'Select 2 Demo', 'elementortestplugin' ),
+				'type'        => \Elementor\Controls_Manager::SELECT2,
+				'label_block' => true,
+				'multiple'    => true,
+				'options'     => [
+					'BD' => __( 'Bangladesh', 'elementortestplugin' ),
+					'BR' => __( 'Brazil', 'elementortestplugin' ),
+					'AR' => __( 'Argentina', 'elementortestplugin' ),
+					'AU' => __( 'Australia', 'elementortestplugin' ),
+					'DK' => __( 'Denmark', 'elementortestplugin' ),
+				],
+			]
+		);
+
+		$this->add_control(
+			'demo_choose',
+			[
+				'label'       => __( 'Choose Demo', 'elementortestplugin' ),
+				'type'        => \Elementor\Controls_Manager::CHOOSE,
+				'label_block' => true,
+				'toggle'      => false,
+				'options'     => [
+					'left'    => [
+						'title' => __( 'Left', 'elementortestplugin' ),
+						'icon'  => 'fa fa-align-left'
+					],
+					'center'  => [
+						'title' => __( 'Center', 'elementortestplugin' ),
+						'icon'  => 'fa fa-align-center'
+					],
+					'right'   => [
+						'title' => __( 'Right', 'elementortestplugin' ),
+						'icon'  => 'fa fa-align-right'
+					],
+					'justify' => [
+						'title' => __( 'Justify', 'elementortestplugin' ),
+						'icon'  => 'fa fa-align-justify'
+					],
+				],
+			]
+		);
+
+		$this->add_control(
+			'demo_dimension',
+			[
+				'label'       => __( 'Dimension', 'elementortestplugin' ),
+				'type'        => \Elementor\Controls_Manager::IMAGE_DIMENSIONS,
+				'description' => __( 'Input Width & Height', 'elementortestplugin' ),
+				'default'     => [
+					'height' => 100,
+					'width'  => 300
+				]
+			]
+		);
+
+		$this->end_controls_section();
+
+
+	}
+
+	protected function render() {
+		$settings    = $this->get_settings_for_display();
+		$heading     = $settings['heading'];
+		$description = $settings['heading_description'];
+		echo "<h1 class='heading'>" . esc_html( $heading ) . "</h1>";
+		echo "<p class='description'>" . wp_kses_post( $description ) . "</p>";
+		//echo wp_get_attachment_image($settings['image']['id'],'medium');
+		echo \Elementor\Group_Control_Image_Size::get_attachment_image_html( $settings, 'imagesz', 'imagex' );
+
+		echo "<div>";
+		$countries = $settings['demo_select2'];
+		print_r( $countries );
+		echo "<br/>";
+		echo $settings['demo_choose'];
+		echo "<br/>";
+		print_r($settings['demo_dimension']);
+		echo "</div>";
+	}
+
+	protected function _content_template() {
+		?>
+        <#
+        var image = {
+        id:settings.imagex.id,
+        url:settings.imagex.url,
+        size:settings.imagesz_size,
+        dimension: settings.imagesz_custom_dimension,
+
+        }
+
+        var imageUrl = elementor.imagesManager.getImageUrl(image);
+        console.log(imageUrl);
+        #>
+
+        <h1 class="heading">{{{settings.heading}}}</h1>
+        <p class="description">{{{settings.heading_description}}}</p>
+        <img src="{{{ imageUrl }}}" alt="">
+        <ul>
+            <#
+            _.each(settings.demo_select2,function(country){ #>
+            <li>{{{ country }}}</li>
+            <# });
+            #>
+        </ul>
+        <div>
+            {{{ settings.demo_choose }}}
+        </div>
+        <div>
+            Width: {{{ settings.demo_dimension.width }}}<br/>
+            Height: {{{ settings.demo_dimension.height }}}
+        </div>
+		<?php
+	}
+
+}
